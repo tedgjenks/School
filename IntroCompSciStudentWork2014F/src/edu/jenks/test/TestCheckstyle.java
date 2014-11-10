2ab2c9d6cf34ef760163354c9e6cab0010537839
Ted <tedgjenks@hotmail.com>
2014-11-10 13:04:34 -0500

    Student work

--- IntroCompSciStudentWork2014F/src/edu/jenks/test/TestCheckstyle.java
+++ IntroCompSciStudentWork2014F/src/edu/jenks/test/TestCheckstyle.java
@@ -1,7 +1,9 @@
 package edu.jenks.test;
 
+import java.io.File;
+
 public class TestCheckstyle {
 	public static void main(String[] args) {
-		new java.io.File("").delete();
+		//new java.io.File("").delete();
 	}
 }
