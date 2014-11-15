2959199ddc6c20e314f933158f6d92c2a2157952
Ted <tedgjenks@hotmail.com>
2014-11-15 11:54:32 -0500

    Banking UML

--- IntroCompSciStudentWork2014F/src/edu/jenks/test/TestCheckstyle.java
+++ IntroCompSciStudentWork2014F/src/edu/jenks/test/TestCheckstyle.java
@@ -1,7 +1,10 @@
 package edu.jenks.test;
 
+import java.io.File;
+
 public class TestCheckstyle {
 	public static void main(String[] args) {
 		new java.io.File("").delete();
+		File file = new File();
 	}
 }
