8dcf2d7366ec3eecd92e9cc544c8b90931de7e66
Ted <tedgjenks@hotmail.com>
2014-11-23 21:10:23 -0500

    Scrambler

---
+++ IntroCSJavaAssignments/src/edu/jenks/dist/cb/scrambler/Scrambler.java
@@ -0,0 +1,35 @@
+package edu.jenks.dist.cb.scrambler;
+
+import java.util.List;
+
+/**
+ * @author Ted
+ *
+ */
+public interface Scrambler {
+	
+	
+	/**
+	 * Scrambles a given word. </br></br>
+	 * <b>Precondition</b>: <code>word</code> is either an empty string or contains only uppercase letters.</br>
+	 * <b>Postcondition</b>: the string returned was created from <code>word</code> as follows:</br>
+	 * - the word was scrambled, beginning at the first letter and continuing from left to right</br>
+	 * - two consecutive letters consisting of "A" followed by a letter that was not "A" were swapped</br>
+	 * - letters were swapped at most once
+	 * @param word the word to be scrambled
+	 * @return the scrambled word (possibly equal to <code>word</code>)
+	 */
+	String scrambleWord(String word);
+	
+	/**
+	 * Modifies <code>wordList</code> by replacing each word with its scrambled version, removing any words that are unchanged as a result of scrambling. </br></br>
+	 * <b>Precondition</b>: <code>wordList</code> contains only non-null objects</br>
+	 * <b>Postcondition</b>:</br>
+	 * - all words unchanged by scrambling have been removed from <code>wordList</code></br>
+	 * - each of the remaining words has been replaced by its scrambled version</br>
+	 * - the relative ordering of the entries in <code>wordList</code> is the same as it was before the method was called
+	 * @param wordList the list of words</br>
+	 */
+	void scrambleOrRemove(List<String> wordList);
+
+}
---
+++ IntroCSJavaSolutions/src/edu/cb/scrambler/jenks/ted/test/WordScramblerRunner.java
@@ -0,0 +1,34 @@
+package edu.cb.scrambler.jenks.ted.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.cb.scrambler.jenks.ted.WordScrambler;
+import static java.lang.System.out;
+
+public class WordScramblerRunner {
+
+	public static void main(String[] args) {
+		WordScrambler ws = new WordScrambler();
+		out.println("Test scrambleWord");
+		out.println(ws.scrambleWord("TAN"));
+		out.println(ws.scrambleWord("ABRACADABRA"));
+		out.println(ws.scrambleWord("WHOA"));
+		out.println(ws.scrambleWord("AARDVARK"));
+		out.println(ws.scrambleWord("EGGS"));
+		out.println(ws.scrambleWord("A"));
+		out.println(ws.scrambleWord(""));
+		out.println("End test scrambleWord");
+		out.println("Test scrambleOrRemove");
+		List<String> wordList = new ArrayList<String>();
+		wordList.add("TAN");
+		wordList.add("ABRACADABRA");
+		wordList.add("WHOA");
+		wordList.add("APPLE");
+		wordList.add("EGGS");
+		ws.scrambleOrRemove(wordList);
+		out.println(wordList);
+		out.println("End test scrambleOrRemove");
+	}
+
+}
