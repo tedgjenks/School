1880c598e2681db64eb5eb52e7e9e5f6eaed8fed
Ted <tedgjenks@hotmail.com>
2014-11-26 06:04:08 -0500

    Thread monitor

---
+++ APCompSciTesting/src/edu/jenks/test/TestableMonitor.java
@@ -0,0 +1,50 @@
+package edu.jenks.test;
+
+public class TestableMonitor implements Runnable {
+	
+	private final ThreadGroup THREAD_GROUP;
+	private final String THREAD_NAME;
+	private Thread thread;
+		private final long MAX_RUNNING_TIME_MILLIS;
+	private final long SLEEP_MILLIS;
+
+	public TestableMonitor(ThreadGroup threadGroup, String threadName, long maxRunningTimeMillis) {
+		THREAD_GROUP = threadGroup;
+		THREAD_NAME = threadName;
+		MAX_RUNNING_TIME_MILLIS = maxRunningTimeMillis;
+		SLEEP_MILLIS = MAX_RUNNING_TIME_MILLIS / 10;
+	}
+	
+	public void start() {
+		if(thread == null) {
+			thread = new Thread(this, THREAD_NAME);
+			thread.start();
+		}
+	}
+
+	@Override
+	public void run() {
+		long startTime = System.currentTimeMillis();
+		boolean threadsRunning = true;
+		do {
+			try {
+				Thread.sleep(SLEEP_MILLIS);
+				int activeCount = THREAD_GROUP.activeCount();
+				threadsRunning = activeCount > 0;
+			} catch (InterruptedException e) {
+				e.printStackTrace(System.err);
+			}
+		} while(threadsRunning && (System.currentTimeMillis() - startTime) < MAX_RUNNING_TIME_MILLIS);
+		int activeCount = THREAD_GROUP.activeCount();
+		if(activeCount > 0) {
+			System.out.println(activeCount + " threads running at time expired in " + THREAD_NAME);
+			THREAD_GROUP.stop();
+		}
+		System.out.println(THREAD_NAME + " monitor end.");
+	}
+
+	public Thread getThread() {
+		return thread;
+	}
+
+}
