145a42cdcc7f79ecfbdec04cf59fccb528d5c913
Ted <tedgjenks@hotmail.com>
2014-12-12 17:27:50 -0500

    Song list edit 2

---
+++ IntroCompSciStudentWork2014F/src/edu/cb/songlist/white/zackery/MusicDownloads.java
@@ -0,0 +1,43 @@
+package edu.cb.songlist.white.zackery;
+
+import java.util.List;
+
+import edu.jenks.dist.cb.songlist.AbstractMusicDownloads;
+import edu.jenks.dist.cb.songlist.DownloadInfo;
+
+
+public class MusicDownloads extends AbstractMusicDownloads {
+
+	public MusicDownloads() {
+		super();
+	}
+		
+		
+		
+
+
+	private void getTimesDownloaded() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public DownloadInfo getDownloadInfo(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateDownloads(List<String> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
