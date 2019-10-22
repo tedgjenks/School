44a260f88feebff00156affe8b260f73e577a917
tedgj <tedgj@192.168.7.138>
2019-10-22 03:55:01 -0400

    ptc ch1

---
+++ MultipleChoice/src/ptc/ch1/A2.java
@@ -0,0 +1,23 @@
+package ptc.ch1;
+public class A2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		q3();
+	}
+	
+	private static void q3() {
+		int a = 0, b = 0, c = 0;
+		a = b;
+		a = 67;
+		//b = 8.7;
+		//a + b = 8;
+		//a * b = 12;
+		c = a - b;
+		//c = a / 2.3;
+		//boolean t = a;
+		a /= 4;
+		a += c;
+	}
+
+}
