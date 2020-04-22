2a85ec053b0387e08cc3ee672b5cd052496d61f9
tedgj <tedgj@192.168.7.138>
2020-04-22 11:47:03 -0400

    Base Files

---
+++ CompSciBaseFiles/src/edu/util/yln/yfn/Hashtable.java
@@ -0,0 +1,79 @@
+package edu.util.yln.yfn;
+
+import edu.jenks.dist.util.Map;
+
+public class Hashtable<K, V> implements Map<K, V> {
+
+	@Override
+	public int getCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getArrayIndexFromKey(Object key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public K[] getKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V[] getValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
